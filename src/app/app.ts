fb8ea51b1ea8b9dc309820708d72e7fc31eb178b
Wev kleyton <kleyton.cpd@gmnail.com>
2025-09-17 15:50:25 -0300

    Criação do rodape

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Cabecalho } from './componentes/cabecalho/cabecalho';
+import { Rodape } from "./componentes/rodape/rodape";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    Cabecalho, Rodape
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
