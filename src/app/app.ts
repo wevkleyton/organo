c539d130d99399296c398557de23f6c19df422f4
Wev kleyton <kleyton.cpd@gmnail.com>
2025-09-18 12:46:56 -0300

    estilizando componentes css e aplicação de propertys binding

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { Cabecalho } from './componentes/cabecalho/cabecalho';
 import { Rodape } from "./componentes/rodape/rodape";
+import { Livro } from './componentes/livro/livro';
 
 @Component({
   selector: 'app-root',
   imports: [
-    Cabecalho, Rodape
+    Cabecalho, Rodape, Livro
   ],
   templateUrl: './app.html',
   styleUrl: './app.css'
